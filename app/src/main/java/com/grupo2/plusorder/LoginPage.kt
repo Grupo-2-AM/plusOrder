d95382433b93bcbb3b30d243c09801dfec8b1ef4
goncalo <goncalo37865@gmail.com>
2022-05-13 00:01:33 +0100

    - login page (incomplete)

---
+++ app/src/main/java/com/grupo2/plusorder/LoginPage.kt
@@ -0,0 +1,12 @@
+package com.grupo2.plusorder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
--- app/src/main/java/com/grupo2/plusorder/MainActivity.kt
+++ app/src/main/java/com/grupo2/plusorder/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.grupo2.plusorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.principal)
+        setContentView(R.layout.main_page)
+
+        val loginBt = findViewById<View>(R.id.loginBt) as Button
+
+        loginBt.setOnClickListener{
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
